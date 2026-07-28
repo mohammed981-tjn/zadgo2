540b522b4e4775529031af145d4798b3e239f18c
mohammed981-tjn <mohammed981@gmail.com>
2026-07-28 11:17:22 +0300

    Update helpers.dart

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -14,6 +14,7 @@ void showError(BuildContext context, String msg) {
 Future<bool?> showConfirmDialog(BuildContext context, {required String title, required String content,
     String confirmLabel = 'تأكيد', Color? confirmColor}) => showDialog<bool>(
   context: context,
+  barrierColor: Colors.black54,
   builder: (_) => AlertDialog(title: Text(title), content: Text(content), actions: [
     TextButton(onPressed: () => Navigator.pop(context, false), child: const Text('إلغاء')),
     ElevatedButton(onPressed: () => Navigator.pop(context, true),
