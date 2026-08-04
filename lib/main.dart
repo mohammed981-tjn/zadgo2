0e626fb1ed2f1364a15bb41519a7c4ce8c41d479
mohammed981-tjn <mohammed981@gmail.com>
2026-08-04 10:40:27 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,4 +18,56 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
       appId: '1:653081498334:android:6d7b3006fc329fda20684f',
       messagingSenderId: '653081498334',
       projectId: 'restaurant-app-ed699',
-      storageBucket: 'restaurant-app-ed699.firebas
+      storageBucket: 'restaurant-app-ed699.appspot.com',
+    ),
+  );
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyBuU7g3vG5enxaebwxGdGgavG5U8cftwd4',
+      appId: '1:653081498334:android:6d7b3006fc329fda20684f',
+      messagingSenderId: '653081498334',
+      projectId: 'restaurant-app-ed699',
+      storageBucket: 'restaurant-app-ed699.appspot.com',
+    ),
+  );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  runApp(const ZadGoApp());
+}
+
+class ZadGoApp extends StatelessWidget {
+  const ZadGoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final service = FirebaseService();
+    return MultiProvider(
+      providers: [
+        Provider<FirebaseService>(create: (_) => service),
+        ChangeNotifierProvider<app_auth.AuthProvider>(
+          create: (_) => app_auth.AuthProvider(service),
+        ),
+        ChangeNotifierProvider<CartProvider>(
+          create: (_) => CartProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        navigatorKey: navigatorKey,
+        title: 'ZadGo',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        builder: (context, child) => Directionality(
+          textDirection: TextDirection.rtl,
+          child: child!,
+        ),
+        home: const SplashScreen(),
+      ),
+    );
+  }
+}
