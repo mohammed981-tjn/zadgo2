b4ddef07a7e39805563b71da1f4a994dea95cbc9
mohammed981-tjn <mohammed981@gmail.com>
2026-08-04 12:35:23 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:provider/provider.dart';
-import 'providers/auth_provider.dart' as app_auth;
-import 'providers/cart_provider.dart';
 import 'providers/firebase_service.dart';
-import 'screens/splash_screen.dart';
-import 'utils/theme.dart';
+import 'screens/restaurant/RestaurantMain.dart';
 
-final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyBuU7g3vG5enxaebwxGdGgavG5U8cftwd4',
-      appId: '1:653081498334:android:6d7b3006fc329fda20684f',
-      messagingSenderId: '653081498334',
-      projectId: 'restaurant-app-ed699',
-      storageBucket: 'restaurant-app-ed699.appspot.com',
-    ),
-  );
-}
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyBuU7g3vG5enxaebwxGdGgavG5U8cftwd4',
-      appId: '1:653081498334:android:6d7b3006fc329fda20684f',
-      messagingSenderId: '653081498334',
-      projectId: 'restaurant-app-ed699',
-      storageBucket: 'restaurant-app-ed699.appspot.com',
-    ),
-  );
-
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
+void main() {
   runApp(const ZadGoApp());
 }
 
@@ -46,28 +12,33 @@ class ZadGoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final service = FirebaseService();
     return MultiProvider(
       providers: [
-        Provider<FirebaseService>(create: (_) => service),
-        ChangeNotifierProvider<app_auth.AuthProvider>(
-          create: (_) => app_auth.AuthProvider(service),
-        ),
-        ChangeNotifierProvider<CartProvider>(
-          create: (_) => CartProvider(),
-        ),
+        ChangeNotifierProvider(create: (_) => FirebaseService()),
       ],
       child: MaterialApp(
-        navigatorKey: navigatorKey,
-        title: 'ZadGo',
         debugShowCheckedModeBanner: false,
-        theme: AppTheme.light,
-        builder: (context, child) => Directionality(
-          textDirection: TextDirection.rtl,
-          child: child!,
+        title: "ZadGo",
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+          ),
         ),
-        home: const SplashScreen(),
+        home: const AppEntry(),
       ),
     );
+  }
+}
+
+class AppEntry extends StatelessWidget {
+  const AppEntry({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const restaurantId = "RESTAURANT_1";
+    return RestaurantMain(restaurantId: restaurantId);
   }
 }
